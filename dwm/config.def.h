3d6b5d3125b81c7cfb3e50b47aff85dfef06a0dc
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-02-01 13:14:25 -0300

    fix: layout shortcuts
    Fixed default layout shortcuts and added a shortcut o a new layout:
    
    fix []=   => MODKEY + t
    
    fix [M]   => MODKEY + m
    
    fix <><   => MODKEY + f
    
    add TTT   => MODKEY + ShiftMask + t

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -116,9 +116,10 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },  // []=
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[13]} }, // ><>
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },  // [M]
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[5]} },  // TTT
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
